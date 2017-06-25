05e92b1209630072e443d7a416919cad1ae77c11
Nirav Dangi <dangi.nirav2000@gmail.com>
2017-06-26 01:32:52 +0530

    Strarting up

---
+++ app/src/main/java/travelapp/tamasya/view/startup/ForgotPasswordActivity.java
@@ -0,0 +1,67 @@
+package travelapp.tamasya.view.startup;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
+import android.view.MenuItem;
+import android.view.View;
+
+import travelapp.tamasya.R;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+
+        setActionBar();
+
+    }
+
+    public void setActionBar(){
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Forgot Password");
+        setSupportActionBar(toolbar);
+
+        // Show the Up button in the action bar.
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            finish();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
+    public void forgotPassword(View view) {
+
+    }
+}
---
+++ app/src/main/java/travelapp/tamasya/view/startup/VerifyActivity.java
@@ -0,0 +1,15 @@
+package travelapp.tamasya.view.startup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import travelapp.tamasya.R;
+
+public class VerifyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify);
+    }
+}
